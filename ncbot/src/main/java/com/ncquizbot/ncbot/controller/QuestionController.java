76d2bd23059d8ea9bd739c7bd139d0076b1cd73c
Egor <yakovlev.es@phystech.edu>
2020-03-03 18:07:45 +0300

    BUILD_FIX config project for heroku: fix v3

--- ncbot/src/main/java/com/ncquizbot/ncbot/controller/QuestionController.java
+++ ncbot/src/main/java/com/ncquizbot/ncbot/controller/QuestionController.java
@@ -12,12 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
-import org.thymeleaf.util.StringUtils;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
