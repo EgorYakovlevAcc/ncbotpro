9f794d40b9c1a0ee49b26cf1f04de4a68b103c85
Egor <yakovlev.es@phystech.edu>
2020-03-05 18:05:58 +0300

    update project

--- ncbot/src/main/java/com/ncquizbot/ncbot/service/impl/OptionServiceImpl.java
+++ ncbot/src/main/java/com/ncquizbot/ncbot/service/impl/OptionServiceImpl.java
@@ -50,6 +50,7 @@ public class OptionServiceImpl implements OptionService {
 
     @Override
     public void editOptionsByQuestionAndContents(Integer questionId, List<String> contents) {
+        LOGGER.info("EGORKA-POMIDORKA");
         Question question = questionService.findQuestionById(questionId);
         List<Option> questionOptionsList = question.getOptions();
         for (Option option : questionOptionsList) {
