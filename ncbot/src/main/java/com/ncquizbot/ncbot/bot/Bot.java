bf6bd87ef7f62fc660442c8082830b1af7163ad4
Egor <yakovlev.es@phystech.edu>
2020-03-11 06:25:24 +0300

    fix Bot

--- ncbot/src/main/java/com/ncquizbot/ncbot/bot/Bot.java
+++ ncbot/src/main/java/com/ncquizbot/ncbot/bot/Bot.java
@@ -1,16 +1,11 @@
 package com.ncquizbot.ncbot.bot;
 
-import com.ncquizbot.ncbot.model.Question;
-import com.ncquizbot.ncbot.model.User;
 import com.ncquizbot.ncbot.service.BotMessageHandler;
-import com.ncquizbot.ncbot.service.QuestionService;
-import com.ncquizbot.ncbot.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
-import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
